a91bd9287d4d25b6b05e79b0b46cf8f10debe444
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2016-03-16 17:05:41 -0400

    Regras de arredondamento #8

--- MatematicaFinanceira.Testes/TestesDeArredondamento.cs
+++ MatematicaFinanceira.Testes/TestesDeArredondamento.cs
@@ -11,5 +11,17 @@ namespace MatematicaFinanceira.Testes
         {
             Assert.AreEqual(0.12m, 0.124m.Arredondado(2));
         }
+
+        [Test]
+        public void Deve_aumentar_quando_digito_depois_do_piso_for_superior_a_5()
+        {
+            Assert.AreEqual(0.16m, 0.156m.Arredondado(2));
+        }
+
+        [Test]
+        public void Deve_aumentar_quando_o_digito_sendo_5_e_tiver_um_numero_diferente_de_zero_a_direita()
+        {
+            Assert.AreEqual(0.13m, 0.125001m.Arredondado(2));
+        }
     }
 }
